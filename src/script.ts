fb3c9b8c76997a9a26aea1aee1c698391ed3ed99
fabrizzio-gz <fabrizzio.gz@gmail.com>
2022-03-07 16:19:17 -0400

    feat: add fetch api response function

--- src/script.ts
+++ src/script.ts
@@ -21,3 +21,37 @@ interface ChartDataObject {
 }
 
 type ChartData = Array<ChartDataObject>;
+
+/*
+ * Dummy function that returns test data
+ */
+const fetchData = (): ApiResponse => {
+  // Actual API call...
+  return [
+    {
+      id: 1293487,
+      name: "KCRW", // radio station callsign
+      tracks: [{ timestp: "2021-04-08", trackName: "Peaches" }],
+    },
+    {
+      id: 12923,
+      name: "KQED",
+      tracks: [
+        { timestp: "2021-04-09", trackName: "Savage" },
+        { timestp: "2021-04-09", trackName: "Savage (feat. Beyonce)" },
+        { timestp: "2021-04-08", trackName: "Savage" },
+        { timestp: "2021-04-08", trackName: "Savage" },
+        { timestp: "2021-04-08", trackName: "Savage" },
+      ],
+    },
+    {
+      id: 4,
+      name: "WNYC",
+      tracks: [
+        { timestp: "2021-04-09", trackName: "Captain Hook" },
+        { timestp: "2021-04-08", trackName: "Captain Hook" },
+        { timestp: "2021-04-07", trackName: "Captain Hook" },
+      ],
+    },
+  ];
+};
